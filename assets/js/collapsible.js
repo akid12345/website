a2656522d5c09b1fba3b315ce07f068841294f86
akid12345 <142635180+akid12345@users.noreply.github.com>
2025-07-30 21:52:03 -0500

    test

---
+++ assets/js/collapsible.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".collapsible").forEach(btn => {
+  btn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
